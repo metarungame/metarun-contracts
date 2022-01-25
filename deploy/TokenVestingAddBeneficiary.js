83aef267fd5c0fd7677e2e5a7c5e533078c50b5c
Metarun Team <dev@metarun.game>
2022-01-25 14:13:00 +0300

    WIP: feat(captable): add vesting records provided by customer

--- deploy/TokenVestingAddBeneficiary.js
+++ deploy/TokenVestingAddBeneficiary.js
@@ -5,20 +5,25 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments, hre })
 
 
   const totalAmount = ethers.utils.parseEther("550000");
-  const interval = 60 * 60; // hourly
-  const startTimestamp = 1642784400 // 2022-01-21T17:00:00+00:00
-  const shortVestingFinishTimestamp = 1642870800 // 2022-01-22T17:00:00+00:00
-  const shortDuration = shortVestingFinishTimestamp - startTimestamp;
-  const longVestingFinishTimestamp = 1642957200 // 2022-01-23T17:00:00+00:00
-  const longDuration = longVestingFinishTimestamp - startTimestamp; 
+  const DAY = 60 * 60 * 24;
+  const MONTH = DAY * 30;
+  const YEAR = DAY * 365;
+  // planned date of Token Generation Event
+  const tge = Date.parse("2022-02-10T00:00+00:00")/1000;
 
-  const beneficiaries = [];
-  beneficiaries.push(["0x00fe7C821d66368bfC78753Baf9B7639D708F662", startTimestamp, shortDuration, parseEther("100000")]);
-  beneficiaries.push(["0x8889Fc00799C3255bF6162917D62acaC9a37F920", startTimestamp, shortDuration, parseEther("100000")]);
-  beneficiaries.push(["0x297Df83B933eE82ff1FfF4004fCcF7bC50e5e18D", startTimestamp, shortDuration, parseEther("100000")]);
-  beneficiaries.push(["0x1179d39f73080d9dDB22812223614428AB1633b5", startTimestamp, longDuration, parseEther("100000")]);
-  beneficiaries.push(["0x97F19E982A457A327f58602F5d48f5B63bf6159a", startTimestamp, longDuration, parseEther("100000")]);
-  beneficiaries.push(["0x2Cd371c8Dbf0a9F69cf9d0f8b15881f5D524cC80", startTimestamp, longDuration, parseEther("50000")]);
+  ecosystem = { description: "Ecosystem" };
+  ecosystem.address = "0x5fCb8f7149E8aD03544157C90E6f81b26933d3a2";
+  ecosystem.amount = totalAmount.mul(15).div(100);
+  ecosystem.cliff = tge; // start unlocking
+  ecosystem.duration = 3 * MONTH; // total unlock time
+
+  reserve = { description: "Company Reserve" };
+  reserve.address = "0x031A55b6156A5FCad6732fa10A6D58092413B6C6";
+  reserve.amount = totalAmount.mul(10).div(100);
+  reserve.cliff = tge + 6 * MONTH; // start unlocking
+  reserve.duration = 4 * YEAR
+
+  const vestingAllocations = [ecosystem, reserve];
 
   const vesting = await deployments.get("TokenVesting");
 
@@ -38,24 +43,28 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments, hre })
     totalAmount
   );
 
-  for (var i = 0; i < beneficiaries.length; i++) {
-    let beneficiary = beneficiaries[i][0];
-    let start = beneficiaries[i][1];
-    let duration = beneficiaries[i][2];
-    let amount = beneficiaries[i][3];
-    console.log("Adding Beneficiary:", beneficiary);
+  for (var i = 0; i < vestingAllocations.length; i++) {
+    let alloc = vestingAllocations[i];
+    console.log("Adding Vesting Allocation for ", alloc.description);
+    console.log("  Address:", alloc.address);
+    console.log("  Amount:", alloc.amount.toString());
+    console.log("  Cliff (start unlocking):", alloc.cliff);
+    console.log("  Duration:", alloc.duration);
+    console.log("  Vested (totally unlocked):", alloc.cliff + alloc.duration);
+
+    // todo: more verbose output
     await execute(
       'TokenVesting',
       { from: deployer, log: true },
       'createVesting',
-      beneficiary,
-      start,
-      interval,
-      duration,
-      amount
+      alloc.address,
+      alloc.cliff,
+      DAY,
+      alloc.duration,
+      alloc.amount
     );
   }
 };
 
-module.exports.tags = ["VestingAddBeneficiaries"];
+module.exports.tags = ["VestingAllocations"];
 module.exports.dependencies = ["MetarunToken", "TokenVesting"];
