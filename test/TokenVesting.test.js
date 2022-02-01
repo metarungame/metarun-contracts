b408e7a5c1d6b554564f7580b88f36e2bc4d3612
Metarun Team <dev@metarun.game>
2022-02-01 12:31:25 +0300

    test: add token contract to vesting test MRN-39

--- test/TokenVesting.test.js
+++ test/TokenVesting.test.js
@@ -8,10 +8,13 @@ describe("TokenVesting", function () {
     this.deployer = this.signers[0];
     this.beneficiary = this.signers[1];
     this.vestingFactory = await ethers.getContractFactory("TokenVesting");
+    this.tokenFactory = await ethers.getContractFactory("MetarunToken");
   });
 
   beforeEach(async function () {
-    this.vesting = await this.vestingFactory.deploy("0x0000000000000000000000000000000000000001");
+    this.token = await this.tokenFactory.deploy();
+    this.vesting = await this.vestingFactory.deploy(this.token.address);
+    await this.token.approve(this.vesting.address, await this.token.totalSupply());
   });
 
   it("beneficiary doesn't have any allocation", async function () {
@@ -20,5 +23,22 @@ describe("TokenVesting", function () {
     expect(vesting[1]).to.equal("0");
     expect(vesting[2]).to.equal("0");
     expect(vesting[3]).to.equal("0");
+  });
+
+  describe("after vesting configured", async function () {
+    const now = "1643707273";
+    const day = 60 * 60 * 24;
+
+    beforeEach(async function () {
+      this.vesting.createVesting(this.beneficiary.address, now, 1, day, 0);
+    });
+
+    it("beneficiary has proper allocation", async function () {
+      const vesting = await this.vesting.getVesting(this.beneficiary.address);
+      expect(vesting[0]).to.equal(now);
+      expect(vesting[1]).to.equal("1");
+      expect(vesting[2]).to.equal(day);
+      expect(vesting[3]).to.equal("0");
+    });
   });
 });
