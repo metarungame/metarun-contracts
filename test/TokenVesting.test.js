7a40868f7d6ef98754c679536774fc6f9f2e72f1
Metarun Team <dev@metarun.game>
2022-02-01 12:17:08 +0300

    test: add test for zero allocation MRN-39

--- test/TokenVesting.test.js
+++ test/TokenVesting.test.js
@@ -1,8 +1,12 @@
 const { ethers } = require("hardhat");
+const { expect } = require("chai");
 const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
 describe("TokenVesting", function () {
   before(async function () {
+    this.signers = await ethers.getSigners();
+    this.deployer = this.signers[0];
+    this.beneficiary = this.signers[1];
     this.vestingFactory = await ethers.getContractFactory("TokenVesting");
   });
 
@@ -10,7 +14,11 @@ describe("TokenVesting", function () {
     this.vesting = await this.vestingFactory.deploy("0x0000000000000000000000000000000000000001");
   });
 
-  it("contract is deployed", async function () {
-    await this.vesting.deployed();
+  it("beneficiary doesn't have any allocation", async function () {
+    const vesting = await this.vesting.getVesting(this.beneficiary.address);
+    expect(vesting[0]).to.equal("0");
+    expect(vesting[1]).to.equal("0");
+    expect(vesting[2]).to.equal("0");
+    expect(vesting[3]).to.equal("0");
   });
 });
