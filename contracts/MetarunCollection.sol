3d4a28eec846278943c157f41fd839ff22e1292e
Metarun Team <dev@metarun.game>
2022-02-02 13:21:39 +0300

    feat: add proper token categories #MRN-36

--- contracts/MetarunCollection.sol
+++ contracts/MetarunCollection.sol
@@ -5,35 +5,33 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract MetarunCollection is ERC1155, AccessControl {
-    uint256 public constant FIRST_TOKEN = 0;
-    uint256 public constant SECOND_TOKEN = 1;
-
-    uint256 public constant TWO_INSTANCE_TOKEN = 2;
+    uint256 public constant CHARACTER = 0;
+    uint256 public constant PET = 1;
+    uint256 public constant ARTIFACT = 2;
+    uint256 public constant SKIN = 3;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     constructor(string memory uri) ERC1155(uri) {
-        _mint(msg.sender, FIRST_TOKEN, 1, "");
-        _mint(msg.sender, SECOND_TOKEN, 1, "");
-        _mint(msg.sender, TWO_INSTANCE_TOKEN, 1, "");
-        _mint(msg.sender, TWO_INSTANCE_TOKEN, 1, "");
-
         _setupRole(MINTER_ROLE, _msgSender());
-        _setupRole(ADMIN_ROLE, _msgSender());
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns(bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
-    function mint(address to, uint256 id, uint256 amount) public {
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount
+    ) public {
         require(hasRole(MINTER_ROLE, _msgSender()), "METARUNCOLLECTION: need MINTER_ROLE");
         _mint(to, id, amount, "");
     }
 
-    function changeUri(string memory newUri) public {
-        require(hasRole(ADMIN_ROLE, _msgSender()), "METARUNCOLLECTION: need ADMIN_ROLE");
+    function setURI(string memory newUri) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "METARUNCOLLECTION: need DEFAULT_ADMIN_ROLE");
         _setURI(newUri);
     }
 }
--- test/MetarunCollection.test.js
+++ test/MetarunCollection.test.js
@@ -1,102 +1,136 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-const URI_TOKEN = "localhost:8000/static/collection/{id}.json";
+const URI_TOKEN = "https://app-staging.metarun.game/metadata/{id}.json";
 
 describe("Metarun token collection", function () {
+  it("is deployed", async function () {
+    this.metarunCollectionFactory = await ethers.getContractFactory("MetarunCollection");
+    this.metarunCollection = await this.metarunCollectionFactory.deploy(URI_TOKEN);
+    await this.metarunCollection.deployed();
+    this.signers = await ethers.getSigners();
+    this.deployer = this.signers[0];
+    this.stranger = this.signers[1];
+    this.recepient = this.signers[2];
+    this.characterToken = await this.metarunCollection.CHARACTER();
+    this.petToken = await this.metarunCollection.PET();
+    this.artifactToken = await this.metarunCollection.ARTIFACT();
+    this.skinToken = await this.metarunCollection.SKIN();
+  });
 
-    it("is deployed", async function () {
-        this.metarunCollectionFactory = await ethers.getContractFactory("MetarunCollection");
-        this.metarunCollection = await this.metarunCollectionFactory.deploy(URI_TOKEN);
-        await this.metarunCollection.deployed();
-        this.signers = await ethers.getSigners();
-        this.deployer = this.signers[0];
-        this.stranger = this.signers[1];
-        this.firstTokenId = await this.metarunCollection.FIRST_TOKEN();
-        this.secondTokenId = await this.metarunCollection.SECOND_TOKEN();
-        this.tokenWithTwoInstances = await this.metarunCollection.TWO_INSTANCE_TOKEN();
+  describe("Token creation", function () {
+    async function testTokenCreation(collectionToken, address, tokenId, expectedAmount) {
+      const balanceOfFirstToken = await collectionToken.balanceOf(address, tokenId);
+      expect(balanceOfFirstToken).to.be.eq(expectedAmount);
+    }
+
+    async function mintToken(collectionToken, minter, receiver, tokenId, amount) {
+      await collectionToken.connect(minter).mint(receiver, tokenId, amount);
+    }
+
+    it("should create character token", async function () {
+      await mintToken(this.metarunCollection, this.deployer, this.stranger.address, this.characterToken, 1);
+      await testTokenCreation(this.metarunCollection, this.stranger.address, this.characterToken, "1");
     });
 
-    describe("Token creation", function () {
-
-        async function testTokenCreation(collectionToken, deployer, tokenId, expectedAmount) {
-            const balanceOfFirstToken = await collectionToken.balanceOf(deployer, tokenId);
-            expect(balanceOfFirstToken).to.be.eq(expectedAmount);
-        }
-
-        it("should create non-fungible tokens correctly", async function () {
-            await testTokenCreation(this.metarunCollection, this.deployer.address, this.firstTokenId, "1");
-            await testTokenCreation(this.metarunCollection, this.deployer.address, this.secondTokenId, "1");
-            await testTokenCreation(this.metarunCollection, this.deployer.address, this.tokenWithTwoInstances, "2");
-        });
-
-        it("should give zero if token does not exist", async function () {
-            const dummyBalance = await this.metarunCollection.balanceOf(this.deployer.address, 9999);
-            expect(dummyBalance).to.be.eq(0);
-        });
+    it("should create pet token", async function () {
+      await mintToken(this.metarunCollection, this.deployer, this.stranger.address, this.petToken, 1);
+      await testTokenCreation(this.metarunCollection, this.stranger.address, this.petToken, "1");
     });
 
-    describe("Token transfer", async function () {
-        it("should perform a transfer correctly", async function () {
-            await this.metarunCollection.safeTransferFrom(this.deployer.address, this.stranger.address, this.firstTokenId, 1, []);
-            const balance = await this.metarunCollection.balanceOf(this.stranger.address, 0);
-            expect(balance).to.be.eq(1);
-        });
-
-        it("should revert transfer more than 1 NFT", async function () {
-            const transfer = this.metarunCollection.connect(this.stranger).safeTransferFrom(this.stranger.address, this.deployer.address, this.firstTokenId, 2, []);
-            await expect(transfer).to.be.revertedWith("ERC1155: insufficient balance for transfer");
-        });
-
-        it("should perform a transfer 2 items of two-instance token", async function () {
-            const transfer = this.metarunCollection.connect(this.deployer).
-                safeTransferFrom(this.deployer.address, this.stranger.address, this.tokenWithTwoInstances, 2, []);
-            await expect(transfer).to.be.ok;
-        });
+    it("should create artifact token", async function () {
+      await mintToken(this.metarunCollection, this.deployer, this.stranger.address, this.artifactToken, 1);
+      await testTokenCreation(this.metarunCollection, this.stranger.address, this.artifactToken, "1");
     });
 
-    describe("Token uri", function () {
-
-        async function testUriGiving(collectionToken, tokenId) {
-            const uri = await collectionToken.uri(tokenId);
-            expect(uri).to.be.eq(URI_TOKEN);
-        }
-
-        it("should correctly give uri for tokens", async function () {
-            testUriGiving(this.metarunCollection, this.firstTokenId);
-            testUriGiving(this.metarunCollection, this.secondTokenId);
-            testUriGiving(this.metarunCollection, this.tokenWithTwoInstances);
-        });
-
+    it("should create skin token", async function () {
+      await mintToken(this.metarunCollection, this.deployer, this.stranger.address, this.skinToken, 1);
+      await testTokenCreation(this.metarunCollection, this.stranger.address, this.skinToken, "1");
     });
 
+    it("should give zero if token does not exist", async function () {
+      const dummyBalance = await this.metarunCollection.balanceOf(this.deployer.address, 9999);
+      expect(dummyBalance).to.be.eq(0);
+    });
+  });
 
-    describe("Token roles", function () {
-        it("should deny minting for non-minter", async function () {
-            const attemptToMint = this.metarunCollection.connect(this.stranger)
-                .mint(this.stranger.address, this.firstTokenId, 10);
-            await expect(attemptToMint).to.be.revertedWith("METARUNCOLLECTION: need MINTER_ROLE");
-        });
-
-        it("should perform minting for minter", async function () {
-            const attemptToMint = this.metarunCollection
-                .mint(this.stranger.address, this.firstTokenId, 10);
-            await expect(attemptToMint).to.be.ok;
-        });
-
-        it("should deny changing uri for non-admin", async function () {
-            const attemptToChangeURI = this.metarunCollection.connect(this.stranger)
-                .changeUri("localhost:5050/api/tokens/{id}.json");
-            await expect(attemptToChangeURI).to.be.revertedWith("METARUNCOLLECTION: need ADMIN_ROLE");
-        });
-
-        it("should perform changing uri for admin", async function () {
-            const attemptToChangeURI = this.metarunCollection
-                .changeUri("localhost:5050/api/tokens/{id}.json");
-            await expect(attemptToChangeURI).to.be.ok;
-        });
-
-
+  describe("Token transfer", async function () {
+    before(async function () {
+      this.senderAddress = this.stranger.address;
+      this.recepientAddress = this.recepient.address;
     });
 
+    async function checkBalancesAfterTx(collectionToken, tokenId, senderAddress, recepientAddress) {
+      const recipientBalanceAfterTx = await collectionToken.balanceOf(recepientAddress, tokenId);
+      const senderBalanceAfterTx = await collectionToken.balanceOf(senderAddress, tokenId);
+      expect(recipientBalanceAfterTx).to.be.eq(1);
+      expect(senderBalanceAfterTx).to.be.eq(0);
+    }
+
+    it("should transfer character token", async function () {
+      await this.metarunCollection
+        .connect(this.stranger)
+        .safeTransferFrom(this.senderAddress, this.recepientAddress, this.characterToken, 1, []);
+      checkBalancesAfterTx(this.metarunCollection, this.characterToken, this.senderAddress, this.recepientAddress);
+    });
+
+    it("should transfer pet token", async function () {
+      await this.metarunCollection.connect(this.stranger).safeTransferFrom(this.senderAddress, this.recepientAddress, this.petToken, 1, []);
+      checkBalancesAfterTx(this.metarunCollection, this.petToken, this.senderAddress, this.recepientAddress);
+    });
+
+    it("should transfer artifact token", async function () {
+      await this.metarunCollection.connect(this.stranger).safeTransferFrom(this.senderAddress, this.recepientAddress, this.artifactToken, 1, []);
+      checkBalancesAfterTx(this.metarunCollection, this.artifactToken, this.senderAddress, this.recepientAddress);
+    });
+
+    it("should transfer skin token", async function () {
+      await this.metarunCollection.connect(this.stranger).safeTransferFrom(this.senderAddress, this.recepientAddress, this.skinToken, 1, []);
+      checkBalancesAfterTx(this.metarunCollection, this.skinToken, this.senderAddress, this.recepientAddress);
+    });
+
+    it("should revert transfer of non-existing token", async function () {
+      const nonExistingToken = 99999;
+      const badTransfer = this.metarunCollection
+        .connect(this.stranger)
+        .safeTransferFrom(this.senderAddress, this.recepientAddress, nonExistingToken, 1, []);
+      await expect(badTransfer).to.be.reverted;
+    });
+  });
+
+  describe("Token uri", function () {
+    async function testUriGiving(collectionToken, tokenId) {
+      const uri = await collectionToken.uri(tokenId);
+      expect(uri).to.be.eq(URI_TOKEN);
+    }
+
+    it("should correctly give uri for tokens", async function () {
+      testUriGiving(this.metarunCollection, this.characterToken);
+      testUriGiving(this.metarunCollection, this.petToken);
+      testUriGiving(this.metarunCollection, this.artifactToken);
+      testUriGiving(this.metarunCollection, this.skinToken);
+    });
+  });
+
+  describe("Token roles", function () {
+    it("should deny minting for non-minter", async function () {
+      const attemptToMint = this.metarunCollection.connect(this.stranger).mint(this.stranger.address, this.characterToken, 10);
+      await expect(attemptToMint).to.be.revertedWith("METARUNCOLLECTION: need MINTER_ROLE");
+    });
+
+    it("should perform minting for minter", async function () {
+      const attemptToMint = this.metarunCollection.mint(this.stranger.address, this.characterToken, 10);
+      await expect(attemptToMint).to.be.ok;
+    });
+
+    it("should deny changing uri for non-admin", async function () {
+      const attemptTosetURI = this.metarunCollection.connect(this.stranger).setURI("localhost:5050/api/tokens/{id}.json");
+      await expect(attemptTosetURI).to.be.revertedWith("METARUNCOLLECTION: need DEFAULT_ADMIN_ROLE");
+    });
+
+    it("should perform changing uri for admin", async function () {
+      const attemptTosetURI = this.metarunCollection.setURI("localhost:5050/api/tokens/{id}.json");
+      await expect(attemptTosetURI).to.be.ok;
+    });
+  });
 });
