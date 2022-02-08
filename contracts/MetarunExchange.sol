14fd6a26bf5e70d2ae66f3d53b58bd4100839be5
Metarun Team <dev@metarun.game>
2022-02-08 12:35:01 +0300

    feat(exchange): add SellOrder and buy method MRN-63

--- contracts/MetarunExchange.sol
+++ contracts/MetarunExchange.sol
@@ -10,8 +10,32 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 contract MetarunExchange {
     IERC1155 public token;
 
+    struct SellOrder {
+        // address of the current tokenholder
+        address seller;
+        // id of ERC-1155 token (kind)
+        uint256 tokenId;
+        // ERC155 amount of given Id user is going to sell
+        uint256 amount;
+        // the point at which order becomes outdated
+        uint256 expirationTime;
+        // price in wei (for entire amount of tokens)
+        uint256 price;
+        // random salt to prevent duplicate hashes
+        uint256 salt;
+    }
+
     constructor(address _token) {
         require(_token != address(0), "token address cannot be zero");
         token = IERC1155(_token);
+    }
+
+    // todo: SECURITY! make non-reentrant!
+    function buy(SellOrder memory sellOrder) external payable {
+        // check the order is actual (not traded already)
+        // check the order is not expired and not too early
+        // check the value matches order's price
+        // withdraw NFTs from seller and transfer to buyer
+        // send value to seller
     }
 }
--- test/MetarunExchange.test.js
+++ test/MetarunExchange.test.js
@@ -10,6 +10,32 @@ describe("Metarun Exchange", function () {
     this.signers = await ethers.getSigners();
     this.deployer = this.signers[0];
     this.stranger = this.signers[1];
+    this.domain = {
+        name: "Metarun",
+        version: "0.1",
+        chainId: this.deployer.provider._network.chainId,
+        verifyingContract: null,
+      };
+  
+      this.types = {
+        SellOrder: [
+          { name: "seller", type: "address" },
+          { name: "tokenId", type: "uint256" },
+          { name: "amount", type: "uint256" },
+          { name: "expirationTime", type: "uint256" },
+          { name: "price", type: "uint256" },
+          { name: "salt", type: "uint256" },
+        ],
+      };
+  
+      this.sellOrder = {
+        seller: this.deployer.address,
+        tokenId: 0,
+        amount: 1,
+        expirationTime: 2,
+        price: 3,
+        salt: 4
+      };
   });
 
   beforeEach(async function () {
