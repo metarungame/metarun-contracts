eb9a08d48b38ff522c6fb58a50d0021d3b545054
Metarun Team <dev@metarun.game>
2022-02-24 22:11:03 +0300

    feat: WIP: vesting with timelock

--- contracts/mocks/TokenVestingMock.sol
+++ contracts/mocks/TokenVestingMock.sol
@@ -5,18 +5,37 @@ import "../TokenVesting.sol";
 contract TokenVestingMock is TokenVesting {
     uint256 private currentBlockTime;
 
-    constructor(address token) TokenVesting(token) {}
+    constructor(
+        address _token,
+        uint256 _lockBps,
+        uint256 _vestBps,
+        uint256 _lockClaimTime,
+        uint256 _vestStart,
+        uint256 _vestDuration,
+        uint256 _vestInterval
+    ) TokenVesting(_token, _lockBps, _vestBps, _lockClaimTime, _vestStart, _vestDuration, _vestInterval) {}
+
+    function setLockBps(uint256 _lockBps) public {
+        lockBps = _lockBps;
+    }
+
+    function setVestBps(uint256 _vestBps) public {
+        vestBps = _vestBps;
+    }
+
+    function setReleased(address _beneficiary, uint256 _released) public {
+        allocations[_beneficiary].released = _released;
+    }
 
     function setCurrentBlockTime(uint256 _currentBlockTime) public {
         currentBlockTime = _currentBlockTime;
     }
 
-    function _getCurrentBlockTime() internal override view returns (uint256) {
+    function _getCurrentBlockTime() internal view override returns (uint256) {
         return currentBlockTime;
     }
 
     function getCurrentBlockTime() public view returns (uint256) {
         return _getCurrentBlockTime();
     }
-
 }
