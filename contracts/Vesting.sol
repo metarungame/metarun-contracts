595e5b99cd5c3e33558d613656f6bf34fc7cf586
Metarun Team <dev@metarun.game>
2022-03-10 12:14:42 +0300

    feat: vesting for reserve, team, bd and marketing

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -324,3 +324,51 @@ contract VestingScaleSwap is Vesting {
         uint256 _vestInterval
     ) Vesting(_token, _lockBps, _vestBps, _lockClaimTime, _vestStart, _vestDuration, _vestInterval) {}
 }
+
+contract VestingCompanyReserve is Vesting {
+    constructor(
+        address _token,
+        uint256 _lockBps,
+        uint256 _vestBps,
+        uint256 _lockClaimTime,
+        uint256 _vestStart,
+        uint256 _vestDuration,
+        uint256 _vestInterval
+    ) Vesting(_token, _lockBps, _vestBps, _lockClaimTime, _vestStart, _vestDuration, _vestInterval) {}
+}
+
+contract VestingTeam is Vesting {
+    constructor(
+        address _token,
+        uint256 _lockBps,
+        uint256 _vestBps,
+        uint256 _lockClaimTime,
+        uint256 _vestStart,
+        uint256 _vestDuration,
+        uint256 _vestInterval
+    ) Vesting(_token, _lockBps, _vestBps, _lockClaimTime, _vestStart, _vestDuration, _vestInterval) {}
+}
+
+contract VestingBdAndPartnerships is Vesting {
+    constructor(
+        address _token,
+        uint256 _lockBps,
+        uint256 _vestBps,
+        uint256 _lockClaimTime,
+        uint256 _vestStart,
+        uint256 _vestDuration,
+        uint256 _vestInterval
+    ) Vesting(_token, _lockBps, _vestBps, _lockClaimTime, _vestStart, _vestDuration, _vestInterval) {}
+}
+
+contract VestingMarketing is Vesting {
+    constructor(
+        address _token,
+        uint256 _lockBps,
+        uint256 _vestBps,
+        uint256 _lockClaimTime,
+        uint256 _vestStart,
+        uint256 _vestDuration,
+        uint256 _vestInterval
+    ) Vesting(_token, _lockBps, _vestBps, _lockClaimTime, _vestStart, _vestDuration, _vestInterval) {}
+}
