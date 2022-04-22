45cd4b9649a7ce23b67b79d0edd9da2f16980c6e
Metarun Team <dev@metarun.game>
2022-04-22 15:51:34 +0300

    test: extend test cases #MRN-286

--- test/MetarunCollection/IncreaseFungible.test.js
+++ test/MetarunCollection/IncreaseFungible.test.js
@@ -18,77 +18,72 @@ describe("MetarunCollection | Increase/Decrease fungible token", function () {
     this.metarunCollection = await metarunCollectionFactory.deploy(URI_TOKEN);
   });
   describe("Increase token Health to character", function () {
+    it("check increaseHealth reverted with reason string 'Does not match token character kind'", async function () {
+      await expect(this.metarunCollection.increaseHealth(amount, HEALTH_TOKEN_ID)).to.be.revertedWith("Does not match token character kind");
+    });
+    it("check decreaseHealth reverted with reason string 'Does not match token character kind'", async function () {
+      await expect(this.metarunCollection.decreaseHealth(amount, HEALTH_TOKEN_ID)).to.be.revertedWith("Does not match token character kind");
+    });
+    it("check increaseHealth reverted with reason string 'Not enough character token balance'", async function () {
+      await expect(this.metarunCollection.increaseHealth(amount, CHARACTER_TOKEN_ID)).to.be.revertedWith("Not enough character token balance");
+    });
+    it("check decreaseHealth reverted with reason string 'Not enough character token balance'", async function () {
+      await expect(this.metarunCollection.decreaseHealth(amount, CHARACTER_TOKEN_ID)).to.be.revertedWith("Not enough character token balance");
+    });
     describe("Mint token Character for account", function () {
       beforeEach(async function () {
         this.metarunCollection.mint(this.deployer.address, CHARACTER_TOKEN_ID, 1);
       });
       it("check token Character balanceOf increased", async function () {
-        expect(
-          await this.metarunCollection.balanceOf(this.deployer.address, CHARACTER_TOKEN_ID)
-        ).to.be.equal(1);
+        expect(await this.metarunCollection.balanceOf(this.deployer.address, CHARACTER_TOKEN_ID)).to.be.equal(1);
       });
-      it("check reverts");
       describe("Mint token Health for account", function () {
         beforeEach(async function () {
           await this.metarunCollection.mint(this.deployer.address, HEALTH_TOKEN_ID, amount);
         });
         it("check token Health balanceOf increased", async function () {
-          expect(
-            await this.metarunCollection.balanceOf(this.deployer.address, HEALTH_TOKEN_ID)
-          ).to.be.equal(amount);
+          expect(await this.metarunCollection.balanceOf(this.deployer.address, HEALTH_TOKEN_ID)).to.be.equal(amount);
         });
-        it("check reverts");
         describe("Increase token Health to character", function () {
           beforeEach(async function () {
             await this.metarunCollection.increaseHealth(amount, CHARACTER_TOKEN_ID);
           });
           it("check token Health balance for account", async function () {
-            expect(
-              await this.metarunCollection.balanceOf(this.deployer.address, HEALTH_TOKEN_ID)
-            ).to.be.equal(0);
+            expect(await this.metarunCollection.balanceOf(this.deployer.address, HEALTH_TOKEN_ID)).to.be.equal(0);
           });
           it("check mappinig tokens Character and Health", async function () {
-            expect(
-              await this.metarunCollection.getHealth(CHARACTER_TOKEN_ID)
-            ).to.be.equal(amount);
+            expect(await this.metarunCollection.getHealth(CHARACTER_TOKEN_ID)).to.be.equal(amount);
           });
-          it("check reverts");
+          it("check reverted with reason string 'Not enough health token balance'", async function () {
+            await expect(this.metarunCollection.increaseHealth(amount, CHARACTER_TOKEN_ID)).to.be.revertedWith(
+              "Not enough health token balance"
+            );
+          });
           describe("Decrease Health token from character", function () {
             beforeEach(async function () {
               await this.metarunCollection.decreaseHealth(amountHalf, CHARACTER_TOKEN_ID);
             });
             it("check amount token Health in mappinig Character and Health", async function () {
-              expect(
-                await this.metarunCollection.getHealth(CHARACTER_TOKEN_ID)
-              ).to.be.equal(amount - amountHalf);
+              expect(await this.metarunCollection.getHealth(CHARACTER_TOKEN_ID)).to.be.equal(amount - amountHalf);
             });
             it("check token Health balance for account", async function () {
-              expect(
-                await this.metarunCollection.balanceOf(this.deployer.address, HEALTH_TOKEN_ID)
-              ).to.be.equal(amount - amountHalf);
+              expect(await this.metarunCollection.balanceOf(this.deployer.address, HEALTH_TOKEN_ID)).to.be.equal(amount - amountHalf);
             });
-            it("check reverts");
+            it("check reverted with reason string 'Does not match token character kind'", async function () {
+              await expect(this.metarunCollection.decreaseHealth(amount, HEALTH_TOKEN_ID)).to.be.revertedWith(
+                "Does not match token character kind"
+              );
+            });
             describe("Transfer token Health to another account", function () {
               beforeEach(async function () {
-                await this.metarunCollection.safeTransferFrom(
-                  this.deployer.address,
-                  this.another.address,
-                  HEALTH_TOKEN_ID,
-                  amountHalf,
-                  []
-                );
+                await this.metarunCollection.safeTransferFrom(this.deployer.address, this.another.address, HEALTH_TOKEN_ID, amountHalf, []);
               });
               it("check token Health balance for account", async function () {
-                expect(
-                  await this.metarunCollection.balanceOf(this.deployer.address, HEALTH_TOKEN_ID)
-                ).to.be.equal(0);
+                expect(await this.metarunCollection.balanceOf(this.deployer.address, HEALTH_TOKEN_ID)).to.be.equal(0);
               });
               it("check token Health balance for another account", async function () {
-                expect(
-                  await this.metarunCollection.balanceOf(this.another.address, HEALTH_TOKEN_ID)
-                ).to.be.equal(amountHalf);
+                expect(await this.metarunCollection.balanceOf(this.another.address, HEALTH_TOKEN_ID)).to.be.equal(amountHalf);
               });
-              it("check reverts");
             });
           });
         });
