308fd56e011fdf2e0fc171eecb90b0067fe95c9d
Metarun Team <dev@metarun.game>
2022-04-29 18:29:02 +0300

    refactor: add minter role for tokens #MRN-305

--- deploy/fixedStaking90Days.js
+++ deploy/fixedStaking90Days.js
@@ -4,8 +4,8 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments }) {
   const { deploy, execute } = deployments
   const { deployer } = await getNamedAccounts()
 
-  const token = (await deployments.get("MetarunToken")).address
-  const tokenCollection = (await deployments.get("MetarunCollection")).address
+  const token = (await deployments.get("MetarunToken"))
+  const tokenCollection = (await deployments.get("MetarunCollection"))
   const stakeDuration = 90 * 24 * 60 * 60
   const rewardRate = 678
   const earlyUnstakeFee = 678
@@ -14,9 +14,9 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments }) {
 
   console.log("Deploying contract:", contractName)
   console.log("Deployer:", deployer)
-  console.log("Arguments:", token, stakeDuration, rewardRate, earlyUnstakeFee, tokenCollection)
+  console.log("Arguments:", token.address, stakeDuration, rewardRate, earlyUnstakeFee, tokenCollection.address)
 
-  const deployResult = await deploy(contractName, {
+  const fixedStaking = await deploy(contractName, {
     from: deployer,
     log: true,
     contract: "FixedStaking",
@@ -25,28 +25,46 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments }) {
         execute: {
             init: {
             methodName: "initialize",
-            args: [token, stakeDuration, rewardRate, earlyUnstakeFee, tokenCollection],
+            args: [token.address, stakeDuration, rewardRate, earlyUnstakeFee, tokenCollection.address],
             },
         }
     },
   });
-  console.log(`${contractName}`+" address:", deployResult.address);
+  console.log(`${contractName}`+" address:", fixedStaking.address);
   
-  const fixedStakingDays = await ethers.getContractAt(deployResult.abi, deployResult.address);
+  const fixedStakingContract = await ethers.getContractAt(fixedStaking.abi, fixedStaking.address);
 
-  if (!(await fixedStakingDays.stakesOpen())) {
+  if (!(await fixedStakingContract.stakesOpen())) {
 
-    console.log(`Start ${contractName}`);
-    const setMrunPerSkinTx = await fixedStakingDays.setMrunPerSkin(mrunPerSkin);
+    console.log("Set setMrunPerSkin()");
+    const setMrunPerSkinTx = await fixedStakingContract.setMrunPerSkin(mrunPerSkin);
     await setMrunPerSkinTx.wait();
-  
-    const setSkinKindTx = await fixedStakingDays.setSkinKind(skinKind);
+    
+    console.log("Set setSkinKind()");
+    const setSkinKindTx = await fixedStakingContract.setSkinKind(skinKind);
     await setSkinKindTx.wait();
 
-    const startTx = await fixedStakingDays.start();
+    console.log(`Start ${contractName}`);
+    const startTx = await fixedStakingContract.start();
     await startTx.wait();
-  }
 
+    
+    const tokenContract = await ethers.getContractAt(token.abi, token.address);
+    let role = await tokenContract.MINTER_ROLE();
+    if (!(await tokenContract.hasRole(role, fixedStakingContract.address))) {
+      console.log("Set grantRole() for MetarunToken");
+      let grantRollTx = await tokenContract.grantRole(role, fixedStakingContract.address);
+      await grantRollTx.wait();
+    }
+
+    const tokenCollectionContract = await ethers.getContractAt(tokenCollection.abi, tokenCollection.address);
+    role = await tokenCollectionContract.MINTER_ROLE();
+    if (!(await tokenCollectionContract.hasRole(role, fixedStakingContract.address))) {
+      console.log("Set grantRole() for MetarunCollection");
+      grantRollTx = await tokenCollectionContract.grantRole(role, fixedStakingContract.address);
+      await grantRollTx.wait();
+    }
+  }
 }
 
 module.exports.tags = [contractName]
